e6c5e3e0b3d1117505275c9f8ab172b114dc445a
ThyMajesty <yesthymajesty@gmail.com>
2016-04-24 20:25:51 +0300

    back

--- index.js
+++ index.js
@@ -1,24 +1,46 @@
-var app = require('http').createServer(handler)
-var io = require('socket.io')(app);
-var fs = require('fs');
+/*
+In the node.js intro tutorial (http://nodejs.org/), they show a basic tcp 
+server, but for some reason omit a client connecting to it.  I added an 
+example at the bottom.
+Save the following server in example.js:
+*/
 
-app.listen(6000);
+/*var net = require('net');
 
-function handler (req, res) {
-  fs.readFile(__dirname + '/index.html',
-  function (err, data) {
-    if (err) {
-      res.writeHead(500);
-      return res.end('Error loading index.html');
-    }
-    res.writeHead(200);
-    res.end(data);
-  });
-}
+var server = net.createServer(function(socket) {
+  socket.write('Echo server\r\n');
+  socket.pipe(socket);
+});
 
-io.on('connection', function (socket) {
-	socket.emit('news', { hello: 'world' });
-	socket.on('azaza', function (data) {
-		console.log(data);
-	});
+server.listen(1337, '127.0.0.1');
+*/
+/*
+And connect with a tcp client from the command line using netcat, the *nix 
+utility for reading and writing across tcp/udp network connections.  I've only 
+used it for debugging myself.
+$ netcat 127.0.0.1 1337
+You should see:
+> Echo server
+*/
+
+/* Or use this example tcp client written in node.js.  (Originated with 
+example code from 
+http://www.hacksparrow.com/tcp-socket-programming-in-node-js.html.) */
+
+var net = require('net');
+
+var client = new net.Socket();
+//client.connect(6000, '192.168.0.104', function() {
+client.connect(9999, 'localhost', function() {
+  console.log('Connected');
+  client.write('Hello, server! Love, Client.\n');
+});
+
+client.on('data', function(data) {
+  console.log('Received: ' + data);
+  client.destroy(); // kill client after server's response
+});
+
+client.on('close', function() {
+  console.log('Connection closed');
 });
